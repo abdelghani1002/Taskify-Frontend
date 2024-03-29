4a5b50213d8328f9ad518d5fe531dc11fd260c14
abdelghaniAit <aaittamghart8@gmail.com>
2024-03-29 15:42:20 +0000

    Fix SVG stroke width in Register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -121,7 +121,7 @@ const Register = ({ isLoggedIn, setUser, setIsLoggedIn, setEmail }) => {
                                     className={inputsStyle.name + " w-full pr-4 pl-9 py-2 dark:bg-gray-900 dark:text-gray-300 outline-none focus:outline-none rounded-md shadow-sm"} />
                                 <span className="absolute pl-2">
                                     <svg className="w-6 h-6 text-gray-600 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-width="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        <path stroke="currentColor" strokeWidth="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                     </svg>
                                 </span>
                             </div>
@@ -182,10 +182,9 @@ const Register = ({ isLoggedIn, setUser, setIsLoggedIn, setEmail }) => {
                                     className={inputsStyle.password_confirmation + " w-full pr-4 pl-9 py-2 dark:bg-gray-900 dark:text-gray-300 outline-none focus:outline-none rounded-md shadow-sm"} />
                                 <span className="absolute pl-2">
                                     <svg className="w-6 h-6 text-gray-600 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                        <path fill-rule="evenodd" d="M10 5a2 2 0 0 0-2 2v3h2.4A7.48 7.48 0 0 0 8 15.5a7.48 7.48 0 0 0 2.4 5.5H5a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1V7a4 4 0 1 1 8 0v1.15a7.446 7.446 0 0 0-1.943.685A.999.999 0 0 1 12 8.5V7a2 2 0 0 0-2-2Z" clip-rule="evenodd" />
-                                        <path fill-rule="evenodd" d="M10 15.5a5.5 5.5 0 1 1 11 0 5.5 5.5 0 0 1-11 0Zm6.5-1.5a1 1 0 1 0-2 0v1.5a1 1 0 0 0 .293.707l1 1a1 1 0 0 0 1.414-1.414l-.707-.707V14Z" clip-rule="evenodd" />
+                                        <path fillRule="evenodd" d="M10 5a2 2 0 0 0-2 2v3h2.4A7.48 7.48 0 0 0 8 15.5a7.48 7.48 0 0 0 2.4 5.5H5a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1V7a4 4 0 1 1 8 0v1.15a7.446 7.446 0 0 0-1.943.685A.999.999 0 0 1 12 8.5V7a2 2 0 0 0-2-2Z" clipRule="evenodd" />
+                                        <path fillRule="evenodd" d="M10 15.5a5.5 5.5 0 1 1 11 0 5.5 5.5 0 0 1-11 0Zm6.5-1.5a1 1 0 1 0-2 0v1.5a1 1 0 0 0 .293.707l1 1a1 1 0 0 0 1.414-1.414l-.707-.707V14Z" clipRule="evenodd" />
                                     </svg>
-
                                 </span>
                             </div>
                         </div>
