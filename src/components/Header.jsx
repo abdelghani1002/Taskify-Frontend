a4ed2abfdb8da0a7413b14d3c7eefb5c0f1b3463
abdelghaniAit <aaittamghart8@gmail.com>
2024-03-29 15:48:13 +0000

    Refactor user dropdown in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,8 +23,8 @@ export default function Header({ user }) {
                         <span className="block text-sm">{user.name}</span>
                         <span className="block truncate text-sm font-medium">{user.email}</span>
                     </Dropdown.Header>
-                    <Dropdown.Item>
-                        <Link to="/logout" className="flex gap-2">
+                    <Dropdown.Item className="p-0">
+                        <Link to="/logout" className="p-2 flex gap-2 w-full">
                             <div>
                                 <svg className="w-6 h-6 rotate-180 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
